c30fe76943d9e8e20ef94f3e44ce7e72a2cf07fa
Luka <LPolic@its.jnj.com>
2025-01-10 12:16:29 +0100

    Add synchronization block.

--- ConcurrencyMultithreadingArchitecture/src/main/java/task2/ThreadSafeCollection.java
+++ ConcurrencyMultithreadingArchitecture/src/main/java/task2/ThreadSafeCollection.java
@@ -4,57 +4,44 @@ import static utils.Constants.sleepForSomeTime;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadSafeCollection {
 
   private final List<Integer> numbers = new ArrayList<>();
-  private final Lock lock = new ReentrantLock();
 
 
   public void writeRandomNumbers() {
     while (true) {
-      executeWithLock(() -> {
+      synchronized (this) {  // Synchronize block to ensure thread safety
         numbers.add((int) (Math.random() * 100));
-      });
+      }
       sleepForSomeTime(100);
     }
   }
 
   public void printSum() {
     while (true) {
-      executeWithLock(() -> {
+      synchronized (this) {  // Synchronize block to ensure thread safety
         System.out.println("Sum: " + numbers.stream().mapToInt(Integer::intValue).sum());
-      });
+      }
       sleepForSomeTime(1000);
     }
   }
 
   public void printSqrtSumOfSquares() {
     while (true) {
-      executeWithLock(() -> {
+      synchronized (this) {  // Synchronize block to ensure thread safety
         int sum = numbers.stream().mapToInt(n -> n * n).sum();
         String message = "Square Root of Sum of Squares: ";
 
         if (sum < 0) {
           sum *= -1;
           System.out.println(message + Math.sqrt(sum) + "i");
+        } else {
+          System.out.println(message + Math.sqrt(sum));
         }
-        else System.out.println(message + Math.sqrt(sum));
-
-      });
+      }
       sleepForSomeTime(1000);
-    }
-  }
-
-
-  private void executeWithLock(Runnable action) {
-    lock.lock();
-    try {
-      action.run();
-    } finally {
-      lock.unlock();
     }
   }
 
