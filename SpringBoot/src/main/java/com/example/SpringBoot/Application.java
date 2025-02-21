1793a295feac86615b306d2c1c19d41b0b4f33fe
Luka <LPolic@its.jnj.com>
2025-02-21 10:21:55 +0100

    Finished task 1.

---
+++ SpringBoot/src/main/java/com/example/SpringBoot/Application.java
@@ -0,0 +1,21 @@
+package com.example.SpringBoot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> System.out.println("Hello World !");
+	}
+
+}
