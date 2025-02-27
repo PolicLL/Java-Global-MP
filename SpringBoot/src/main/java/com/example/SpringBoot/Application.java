62d56218c81e3b779e5df8515bb7d361e4536a6e
Luka <LPolic@its.jnj.com>
2025-02-27 09:44:44 +0100

    Added different configurations.

--- SpringBoot/src/main/java/com/example/SpringBoot/Application.java
+++ SpringBoot/src/main/java/com/example/SpringBoot/Application.java
@@ -1,14 +1,25 @@
 package com.example.SpringBoot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
+	@Value("${app.environment}")
+	private String environment;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner printEnvironment() {
+		return args -> System.out.println("Running in environment: " + environment);
 	}
 
 }
