b56cb30a0717c0f177935f990d553322ddf72df1
Luka <LPolic@its.jnj.com>
2025-03-12 13:00:16 +0100

    Setup structure, docker for microservices

---
+++ Microservices/micro-collector/src/main/java/com/example/micro_collector/MicroCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCollectorApplication.class, args);
+	}
+
+}
